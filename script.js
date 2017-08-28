b38e69a6f9233e284b3f5f07fd363137b96b5d00
Didymus <didymus.benson@gmail.com>
2017-08-28 09:53:30 -0600

    iframe added?

--- script.js
+++ script.js
@@ -20,10 +20,67 @@ function convert() {
             break;
     }
 
-    embedcode += videoid + "' data-platform='" + type + "' data-size='" + size + "' data-ratio='" + aspect + "' data-transcriptHTML='" + htmlt + "' data-transcriptRTF='" + rtft + "' ></div> ";
+    embedcode += videoid + "' data-platform='" + type + "' data-size='" + size + "' data-ratio='" + aspect + "' data-transcriptHTML='" + htmlt + "' data-transcriptRTF='" + rtft + "' >";
+
+    embedcode += make_iframe();
+
+    embedcode += " </div> ";
 
     output.innerHTML = (embedcode);
+
+    function make_iframe() {
+        var iframe_code = "";
+
+        var height, width;
+        switch (aspect) {
+            case "16-9":
+                switch (size) {
+                    case "sm":
+                        height = 180;
+                        width = 320;
+                        break;
+                    case "med":
+                        height = 270;
+                        width = 480;
+                        break;
+                    case "lg":
+                        height = 360;
+                        width = 640;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case "4-3":
+                switch (size) {
+                    case "sm":
+                        height = 240;
+                        width = 320;
+                        break;
+                    case "med":
+                        height = 360;
+                        width = 480;
+                        break;
+                    case "lg":
+                        height = 480;
+                        width = 640;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+        }
+        if (type == "youtube") {
+            iframe_code = "<iframe width=" + width + " height=" + height + " src='https://www.youtube.com/embed/" + videoid + "' frameborder='0 ' allowfullscreen></iframe><p>(<a href='" + htmlt + "'>HTML Transcript</a>, <a href='" + rtf + "'>RTF Transcript</a>)</p>";
+        } else if (type == "kaltura") {
+            iframe_code = "<iframe width=" + width + " height=" + height + " src='https://cdnapisec.kaltura.com/p/1157612/sp/115761200/embedIframeJs/uiconf_id/29018071/partner_id/1157612?iframeembed=true&amp;playerId=kaltura_player_1485805514&amp;entry_id=" + videoid + "&amp;flashvars[streamerType]=auto' frameborder='0 ' allowfullscreen=''></iframe><p>(<a href='" + htmlt + "'>HTML Transcript</a>, <a href='" + rtft + "'>RTF Transcript</a>)</p>";
+        }
+        return iframe_code;
+    }
 }
+
+
+
 
 function copy(element) {
     var $temp = $("<input>");
@@ -32,6 +89,7 @@ function copy(element) {
     document.execCommand("copy");
     $temp.remove();
 }
+
 
 function easteregg() {
     document.getElementById("egg").src += "?&autoplay=1&t=2s";
